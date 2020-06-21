ccc7ecc8eb314f75173cef1c053be5f1da3a428c
chaitravi-ce <chaitrav007@gmai.com>
2020-06-21 20:11:27 +0530

    Updates

--- taxi_app/lib/models/location_helper.dart
+++ taxi_app/lib/models/location_helper.dart
@@ -4,11 +4,12 @@ import 'package:http/http.dart' as http;
 
 const GOOGLE_API_KEY = 'AIzaSyBABWJyXQo4ryHl14UEPHIiO2Huqvs8emY';
 
-class LocationHelper {
+class LocationHelper { 
   static String generateLocationPreviewImage({double latitude, double longitude,}) {
     print(latitude);
     print(longitude);
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+    String url = 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=400x400&markers=color:red%7Clabel:S%7C11211%7C11206%7C11222&key=$GOOGLE_API_KEY';
+    return url;
   }
 
   static Future<String> getPlaceAddress(double lat, double lng) async {
